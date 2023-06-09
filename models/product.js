95d18e821fe925c0fb0b446ed43e0e0da2fade4c
abbi964 <abhinavthapliyal964@gmail.com>
2023-06-09 15:30:44 +0530

    Creating the Users Model

--- models/product.js
+++ models/product.js
@@ -75,27 +75,4 @@ class Product {
 
 }
 
-
-// const Product = sequelize.define('product', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   title: Sequelize.STRING,
-//   price: {
-//     type: Sequelize.DOUBLE,
-//     allowNull: false
-//   },
-//   imageUrl: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   description: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   }
-// });
-
 module.exports = Product;
