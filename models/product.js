9a6086d72bee107dd8a1e38736f382aa2998f0a8
abbi964 <abhinavthapliyal964@gmail.com>
2023-06-09 12:59:28 +0530

    Integrating CRUD with MongoDB

--- models/product.js
+++ models/product.js
@@ -1,21 +1,75 @@
+const mongodb = require('mongodb')
 const getDB = require('../util/database').getDB;
 
 class Product {
-  constructor(title,price,description,imageUrl){
+  constructor(title,price,description,imageUrl,id){
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save(){
     const db = getDB();
-    return db.collection('products').insertOne(this)
-      .then((result)=>{
-        console.log(result);
+    if(this._id){
+      // update the product
+      return db.collection('products').updateOne({_id : new mongodb.ObjectId(this._id)},{$set : this})
+        .then((result)=>{
+          console.log(result);
+        })
+        .catch((err)=>{
+          console.log(err);
+        })
+    }
+    else{
+      // make new product
+      return db.collection('products').insertOne(this)
+        .then((result)=>{
+          console.log(result);
+        })
+        .catch((err)=>{
+          console.log(err);
+        })
+    }
+  }
+
+  static fetchAll(){
+    const db = getDB();
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then(products=>{
+        console.log(products)
+        return products
       })
-      .catch((err)=>{
-        console.log(err);
+      .catch(err=>{
+        console.log(err)
+      })
+  }
+
+  static findById(prodId){
+    const db = getDB();
+    return db.collection('products')
+      .find({_id : new mongodb.ObjectId(prodId)})   // as _id provided by mongoDB is "ObjectId" type and not a string
+      .next()
+      .then(product=>{
+        console.log(product)
+        return product
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+  }
+
+  static deleteById(prodId){
+    const db = getDB();
+    return db.collection('products').deleteOne({_id : new mongodb.ObjectId(prodId)})
+      .then(result =>{
+        console.log(result)
+      })
+      .catch(err => {
+        console.log(err)
       })
   }
 
