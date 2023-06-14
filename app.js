9d6bd3b3d801b9744fd1d6712fa07661c4412e28
abbi964 <abhinavthapliyal964@gmail.com>
2023-06-14 13:55:04 +0530

    making user model

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -22,15 +22,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('648311fe207c8cb7210a3fbb')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
+app.use((req, res, next) => {
+  User.findById('64896cbeb2353eed960ae2fd')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 
-// });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -39,6 +39,20 @@ app.use(errorController.get404);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.vghsxbb.mongodb.net/shop?retryWrites=true`)
   .then(result =>{
+    // makign a dummy user if there is no user
+    User.findOne()
+      .then(user =>{
+        if(!user){
+          let user = new User({
+            name : 'Abhinav',
+            email : 'test@test.com',
+            cart : { items : []}
+          });
+          user.save()
+        }
+      })
+
+    // connecting to DB and starting server
     console.log('Connected to mongoDB using mongoose');
     app.listen(3000)
   })
